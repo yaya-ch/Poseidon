aeb20b697164a8417e13a9f0edd83e89d25975c8
yaya <cherifi.yahia@yahoo.fr>
2020-11-16 23:47:03 +0100

    CurvePoint entity refactored. CurvePointDTO created.

--- src/main/java/com/nnk/poseidon/dto/CurvePointDTO.java
+++ src/main/java/com/nnk/poseidon/dto/CurvePointDTO.java
@@ -1,97 +1,56 @@
-package com.nnk.poseidon.domain;
+package com.nnk.poseidon.dto;
 
-import lombok.NoArgsConstructor;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 /**
- * This class groups curvePoint related information.
+ * This class groups information related to curve point.
  *
  * @author Yahia CHERIFI
  */
 
-@Entity
-@Table(name = "curve_point")
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PUBLIC)
-@ToString
-public class CurvePoint {
+public class CurvePointDTO {
 
     /**
      * CurvePoint id.
      */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "curve_point_id")
     private Integer curvePointId;
 
     /**
      * curve id.
      */
     @NotNull(message = "Curve id is mandatory")
-    @Column(name = "curve_id")
     private Integer curveId;
 
     /**
      * as of date.
      */
-    @Column(name = "as_of_date")
     private Timestamp asOfDate;
 
     /**
      * term.
      */
     @NotNull(message = "Term is mandatory")
-    @Column(name = "term")
     private Double term;
 
     /**
      * value.
      */
     @NotNull(message = "Value id is mandatory")
-    @Column(name = "value")
     private Double value;
 
     /**
      * creation date.
      */
-    @Column(name = "creation_date")
     private Timestamp creationDate;
-
-    /**
-     * Class constructor.
-     * @param cCurveId curveId
-     * @param cAsOfDate asOfDate
-     * @param cTerm term
-     * @param cValue value
-     * @param cCreationDate creationDate
-     */
-    public CurvePoint(@NotNull(message = "Curve id is mandatory")
-                      final Integer cCurveId,
-                      final Timestamp cAsOfDate,
-                      @NotNull(message = "Term is mandatory")
-                      final Double cTerm,
-                      @NotNull(message = "Value id is mandatory")
-                      final Double cValue,
-                      final Timestamp cCreationDate) {
-        this.curveId = cCurveId;
-        this.asOfDate = new Timestamp(cAsOfDate.getTime());
-        this.term = cTerm;
-        this.value = cValue;
-        this.creationDate = new Timestamp(cCreationDate.getTime());
-    }
 
     /**
      * asOfDate getter.
