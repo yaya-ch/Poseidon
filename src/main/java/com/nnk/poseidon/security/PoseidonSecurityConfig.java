5cc813411125c094c1e5b2d3fbd8a751719f95b1
yaya <cherifi.yahia@yahoo.fr>
2020-12-24 23:29:25 +0100

    Security updated. RestTemplate bean added to the Poseidon config

--- src/main/java/com/nnk/poseidon/security/PoseidonSecurityConfig.java
+++ src/main/java/com/nnk/poseidon/security/PoseidonSecurityConfig.java
@@ -61,7 +61,7 @@ public class PoseidonSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/admin/**", "/app/**",
                         "/user/**", "/**/delete/**").hasRole("ADMIN")
                 .antMatchers("/bidList/**", "/curvePoint/**",
